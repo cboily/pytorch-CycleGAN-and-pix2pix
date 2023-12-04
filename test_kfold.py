4cf56a8290eb83e6c008f11d88ba6aebc0e76cd8
Claudia Boily <claudia.boily@outlook.com>
2023-12-04 16:22:39 +0100

    correct_error_reg_next_patient

--- test_kfold.py
+++ test_kfold.py
@@ -506,7 +506,7 @@ with torch.no_grad():
                             if zero_fraction <= 0.05:
                                 metrics_reg = calculate_metrics(
                                     aligned_fakeB_array_full_t[:, :, slice, :, :],  #
-                                    sliding_stack_realB_t[
+                                    sliding_stack_realB[
                                         :, :, slice, :, :
                                     ],  # aligned_realB_array_full_t[:, :, slice, :, :],  #
                                 )
@@ -533,7 +533,7 @@ with torch.no_grad():
                 stacked_fakeBs_bone.append(fakeB_bone)
                 stacked_realBs_bone.append(realB_bone)
                 stacked_name_file.append(data_name)
-                #print(prev_patient_name, len(stacked_fakeBs), fakeB.shape, fakeB.dtype)
+                # print(prev_patient_name, len(stacked_fakeBs), fakeB.shape, fakeB.dtype)
                 # Ensure the stack doesn't exceed a length of 7
                 if len(stacked_fakeBs) > 7:
                     stacked_fakeBs.pop(0)
